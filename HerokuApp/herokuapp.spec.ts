95260c2f3e594c94604a0364fadc6d1b1dbb364e
n8zzle <playwithn8zzle@gmail.com>
2025-04-17 13:10:50 +0200

    heroku start

--- HerokuApp/herokuapp.spec.ts
+++ HerokuApp/herokuapp.spec.ts
@@ -8,7 +8,25 @@ test.describe("HerokuApp Testing", () => {
     await page.goto("https://the-internet.herokuapp.com/");
   });
 
-  test("chech page title", async ({ page }) => {
+  test("Check page title", async ({ page }) => {
     await expect(page).toHaveTitle("The Internet");
+  })
+
+  test("Check that all components exist", async ({ page }) => {
+    //Header
+    await expect(page.getByText('Welcome to the-internet')).toBeVisible();
+    await expect(page.getByText('Available Examples')).toBeVisible();
+
+    //Links
+
+    await expect(page.getByRole('link', { name: 'A/B Testing' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'A/B Testing' })).toHaveAttribute('href', '/abtest');
+
+    await expect(page.getByRole('link', { name: 'Add/Remove Elements' })).toBeVisible()
+    await expect(page.getByRole('link', { name: 'Add/Remove Elements' })).toHaveAttribute('href', '/add_remove_elements');
+
+
+    //Footer
+    await expect(page.locator("#page-footer")).toBeVisible()
   })
 })
